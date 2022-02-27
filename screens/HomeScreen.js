b216440e9ba8a8daa3eb9b346579b301ca927fae
sayansarkhel <76253744+sayansarkhel@users.noreply.github.com>
2022-02-27 22:42:10 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,8 +17,8 @@ export default class HomeScreen extends Component{
   getWord=(word)=>{
     var searchKeyword=word.toLowerCase()
     var url;
-    url = "https://rupinwhitehatjr.github.io/dictionary/searchKeyword.json"
-    //url = "https://rupinwhitehatjr.github.io/dictionary/"+ +".json"
+    //url ="https://rupinwhitehatjr.github.io/dictionary/searchKeyword.json"
+    url = "https://rupinwhitehatjr.github.io/dictionary/"+ +".json"
     //url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
     //url = "https://rupinwhitehatjr.github.io/dictionary/"+word+".json"
 
